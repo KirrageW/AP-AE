b77e139b3eae9a272b85d54f6bb8a7a4cfbd926b
William Kirrage <2354535k@student.gla.ac.uk>
2018-02-13 15:37:18 +0000

    not moving

--- Vehicle.java
+++ Vehicle.java
@@ -6,6 +6,14 @@ public class Vehicle extends Thread {
 	private int speed; // how long it will wait in each grid
 	private int size; // different types of vehicle may occupy more than one lane!
 	private String representation;
+	private Grid location;
+	
+	public Vehicle(int direction, int speed, int size, String rep) {
+		this.direction = direction;
+		this.speed = speed;
+		this.size = size;
+		this.representation = rep;
+	}
 	
 	public int getDirection() {
 		return direction;
@@ -26,11 +34,18 @@ public class Vehicle extends Thread {
 	public String getRepresentation() {
 		return representation;
 	}
+	
+	public void move(Grid x ) {  // need to get them to move
+		x.accomodateGridSquare(this);
+		
+		
+		
+	}
 
 	
 	public void run() {
 		try {
-			Grid.accomodateGridSquare(this); // so does need some coordinates - but would this be from the array made in main.... ??
+			location.accomodateGridSquare(this); // so does need some coordinates - but would this be from the array made in main.... ??
 		}
 		
 		
